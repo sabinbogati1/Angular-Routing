dadba88331c6b45b10d6c64e8a1698657e224672
sabinbogati1 <sabin.bogati@studentpartner.com>
2018-12-27 16:42:29 +0545

    Using Query Parameters- practise

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -11,7 +11,10 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
@@ -22,5 +25,10 @@ export class ServerComponent implements OnInit {
       }
     )
   }
+
+  onEdit() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+  }
+
 
 }
