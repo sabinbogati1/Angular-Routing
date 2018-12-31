960224dd9c9a9fd9131ec23d346238e43431fcab
sabinbogati1 <sabin.bogati@studentpartner.com>
2018-12-31 13:10:29 +0545

    Added hash # to routes..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -36,8 +36,9 @@ const appRoutes: Routes = [
   ];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(appRoutes)
+  imports: [
+   // RouterModule.forRoot(appRoutes, {useHash: true})
+    RouterModule.forRoot(appRoutes)
     ],
     exports:[RouterModule]
 })
